122a26f22e4ec63fa8461d80360e96e168f3ea30
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-02-13 12:18:13 -0300

    Add new URL paths for historico and ranking_geral

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('registrar/', views.registrar_km, name='registrar'),
+    path('historico/', views.historico, name='historico'),
+    path('ranking-geral/', views.ranking_geral, name='ranking_geral'),
 ]
