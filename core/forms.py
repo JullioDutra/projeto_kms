804edbb2f7196da3cc4e31b2651e859eee35b353
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-02-17 15:46:13 -0300

    Add 'pace' field to AtividadeForm
    Added 'pace' field to AtividadeForm and updated quantity_km widget.

--- core/forms.py
+++ core/forms.py
@@ -4,9 +4,10 @@ from .models import Atividade
 class AtividadeForm(forms.ModelForm):
     class Meta:
         model = Atividade
-        fields = ['nome_usuario', 'quantidade_km', 'foto_comprovante']
+        fields = ['nome_usuario', 'quantidade_km', 'pace', 'foto_comprovante'] # Adicionado pace aqui
         widgets = {
             'nome_usuario': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seu Nome'}),
-            'quantidade_km': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ex: 5.50'}),
+            'quantidade_km': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ex: 5.50', 'step': '0.01'}),
+            'pace': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex: 05:30 (Opcional)'}),
             'foto_comprovante': forms.FileInput(attrs={'class': 'form-control'}),
         }
