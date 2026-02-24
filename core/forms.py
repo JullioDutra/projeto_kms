9b6979f85d0cf79dedfc5abbd818ddebbb34fb21
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-02-24 09:01:39 -0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -4,9 +4,10 @@ from .models import Atividade
 class AtividadeForm(forms.ModelForm):
     class Meta:
         model = Atividade
-        fields = ['nome_usuario', 'quantidade_km', 'pace', 'foto_comprovante'] # Adicionado pace aqui
+        fields = ['nome_usuario', 'tipo', 'quantidade_km', 'pace', 'foto_comprovante']
         widgets = {
             'nome_usuario': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seu Nome'}),
+            'tipo': forms.Select(attrs={'class': 'form-select'}),
             'quantidade_km': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ex: 5.50', 'step': '0.01'}),
             'pace': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ex: 05:30 (Opcional)'}),
             'foto_comprovante': forms.FileInput(attrs={'class': 'form-control'}),
