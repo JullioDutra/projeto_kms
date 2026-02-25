6ebddd35b30225ddebe3981ea0e68ac8bb16d1d1
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-02-24 22:37:30 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Atividade, MetaMensal, TokenStrava
+from .models import Atividade, MetaMensal, TokenStrava, TempoRota
 admin.site.register(Atividade)
 admin.site.register(MetaMensal)
 admin.site.register(TokenStrava)
+admin.site.register(TempoRota)
