09ff54c08057ed941118a872d5d662f6de4a915f
Jullio Cesar <134459685+JullioDutra@users.noreply.github.com>
2026-02-26 09:35:41 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('rotas/<int:id>/', views.ver_rota, name='ver_rota'),
     path('rotas/<int:id>/excluir/', views.excluir_rota, name='excluir_rota'),
     path('editar-descricao/<int:id>/', views.editar_descricao, name='editar_descricao'),
+    # --- ARENA DE DESAFIOS ---
+    path('arena/', views.arena_desafios, name='arena_desafios'),
+    path('arena/novo/', views.criar_desafio, name='criar_desafio'),
+    path('arena/responder/<int:desafio_id>/<str:resposta>/', views.responder_desafio, name='responder_desafio'),
     # NOVAS ROTAS DO STRAVA
     path('strava/login/', views.strava_login, name='strava_login'),
     path('strava/callback/', views.strava_callback, name='strava_callback'),
